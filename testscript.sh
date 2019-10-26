95a2852941e5a5b624f759a187e20ca797279144
Georgy Nedyalkov <GeorgyNedyalkov@users.noreply.github.com>
2019-10-26 19:52:03 +0300

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,3 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "this was run automagically"
-echo "new line
-echo "this is a new line, once again"
-echo "jenkins"
